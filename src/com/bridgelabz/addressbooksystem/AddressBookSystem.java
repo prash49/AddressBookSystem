5cd579a788a895711bda0c74c1b69164fed7173b
Prashanth <imprash4u@gmail.com>
2021-09-06 08:20:04 +0530

    Added: Created contacts book

--- src/com/bridgelabz/addressbooksystem/AddressBookSystem.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookSystem.java
@@ -1,9 +1,33 @@
 package com.bridgelabz.addressbooksystem;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class AddressBookSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Address Book Program");
+        System.out.println("Enter the First Name");
+        String firstName = sc.next();
+        System.out.println("Enter the Last Name");
+        String lastName = sc.next();
+        System.out.println("Enter The Address");
+        String address = sc.next();
+        System.out.println("Enter the city");
+        String city = sc.next();
+        System.out.println("Enter the state");
+        String state = sc.next();
+        System.out.println("Enter the zip Code");
+        int zip = sc.nextInt();
+        System.out.println("Enter the Phone number");
+        long phone = sc.nextLong();
+        List<ContactsBook> contactList = new ArrayList<>();
+        ContactsBook contact = new ContactsBook(firstName, lastName, address, city, state, zip, phone);
+        contactList.add(contact);
+
     }
+
+}
 }
---
+++ src/com/bridgelabz/addressbooksystem/ContactsBook.java
@@ -0,0 +1,62 @@
+package com.bridgelabz.addressbooksystem;
+
+public class ContactsBook {
+    private String firstName;
+    private String lastName;
+    private String Address;
+    private String city;
+    private String state;
+    private int zip;
+    private long phone;
+
+    ContactsBook(String firstName, String lastName, String address, String city, String state, int zip, long phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getAddress() {
+        return Address;
+    }
+    public void setAddress(String address) {
+        Address = address;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public int getZip() {
+        return zip;
+    }
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+    public long getPhone() {
+        return phone;
+    }
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+}

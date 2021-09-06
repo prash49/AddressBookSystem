12051525179cd6295e682da27d45398b864f2db2
Prashanth <imprash4u@gmail.com>
2021-09-06 08:14:29 +0530

    Added: welcome message

---
+++ src/com/bridgelabz/addressbooksystem/AddressBookSystem.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbooksystem;
+
+import java.util.ArrayList;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program ");
+    }
+}
